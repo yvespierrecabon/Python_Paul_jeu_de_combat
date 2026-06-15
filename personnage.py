0db5cc2f447bddcb365d627f403429e29aa623e7
Yves Cabon <yves.cabon@ypc-world.com>
2026-06-15 18:59:38 +0200

    modif 2 de la classe Personnage

--- personnage.py
+++ personnage.py
@@ -1,15 +1,17 @@
-from typing import List, Optional
+from typing import List
 from pouvoir import Pouvoir
 import random
 
 class Personnage:
-    def __init__(self, nom: str, _categorie: str, _pv: int, pouvoirs: Optional[List[Pouvoir]] = None,
-                 artefacts: List[str] = ['Artefact initial']):
+    def __init__(self, nom: str, _categorie: str, _pv: int, pouvoirs: list[Pouvoir] | None = None,
+                 artefacts:List[str]|None=None):
+        if artefacts is None:
+            artefacts = ['Artefact initial']
         self.nom = nom
         self._categorie = _categorie
         self._pv = _pv
         self._pv_max = _pv
-        self.pouvoirs = pouvoirs
+        self.pouvoirs = pouvoirs or []
         self.artefacts = artefacts
 
     def est_vivant(self):
@@ -23,10 +25,10 @@ class Personnage:
 
     def attaquer(self, cible:'Personnage')->str:
         degat = 5
-        cible_pv = cible._pv
+        ancien_pv = cible._pv
         if self.pouvoirs:
             current_pouvoir = random.choice(self.pouvoirs)
             degat = current_pouvoir.degats
         cible.recevoir_degats(degat)
-        return self.nom+'('+self._categorie+') lance un sort sur '+cible.nom+'('+str(cible_pv)+') -- PV '+cible.nom+ ' : '+ str(cible._pv)
+        return self.nom+'('+self._categorie+') lance un sort sur '+cible.nom+'('+str(ancien_pv)+') -- PV '+cible.nom+ ' : '+ str(cible._pv)
 
